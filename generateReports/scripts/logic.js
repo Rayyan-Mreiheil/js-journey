7e3998340f824f1703235ddf548b859674a609e8
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-17 01:06:44 +0300

    Use <> forEach method to loop over each student in an array

--- generateReports/scripts/logic.js
+++ generateReports/scripts/logic.js
@@ -3,6 +3,12 @@ function generateReports(students) {
 
     const studentsArray = []; // O(1) - Constant time to initialize the array
 
+    students.forEach(person =>{ // O(n) - Loop over each student (n = number of students)
+
+        
+
+    });
+
     
 
 }
