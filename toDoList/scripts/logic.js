420d389131dba998cb843681b47901cf29ebcef6
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-17 02:40:49 +0300

    Finish <> creating a function to read the input value, add it as a new <li> element to the list and then clear the input

--- toDoList/scripts/logic.js
+++ toDoList/scripts/logic.js
@@ -1,6 +1,6 @@
 
 
-
+// function to read the input value, add it as a new <li> element to the list and then clear the input
 function AddToList(){
 
     const task = document.getElementById("task-input").value;
