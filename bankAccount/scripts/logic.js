595da67154244f585a9f8da17d757e47b8a1acd8
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-17 01:45:35 +0300

    Create <> a constructor

--- bankAccount/scripts/logic.js
+++ bankAccount/scripts/logic.js
@@ -1,3 +1,9 @@
 class BankAccount {
+    constructor(ownerName, initialBalance){
+        this.ownerName = ownerName;  // O(1)
+        this.initialBalance =initialBalance;  // O(1)
+        this.history = []; // O(1) – empty array initialization
+    }
+
     
 }
